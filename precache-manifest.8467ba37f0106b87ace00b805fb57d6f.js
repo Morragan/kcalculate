9ab14f2a5a6e4f08c4e1dff5a12d825b52f41abc
Jakub Butkiewicz <jakub.butkiewicz@acaisoft.com>
2020-02-25 00:51:02 +0100

    Updates

---
+++ precache-manifest.8467ba37f0106b87ace00b805fb57d6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e6d6ddf280c630571086608180f6452",
+    "url": "https://morragan.github.io/kcalculate/index.html"
+  },
+  {
+    "revision": "4467b8d8ad2b62d3bb03",
+    "url": "https://morragan.github.io/kcalculate/static/css/2.169712ab.chunk.css"
+  },
+  {
+    "revision": "e8866144a4bd09462214",
+    "url": "https://morragan.github.io/kcalculate/static/css/main.fcf1da21.chunk.css"
+  },
+  {
+    "revision": "4467b8d8ad2b62d3bb03",
+    "url": "https://morragan.github.io/kcalculate/static/js/2.a9bc741a.chunk.js"
+  },
+  {
+    "revision": "e8866144a4bd09462214",
+    "url": "https://morragan.github.io/kcalculate/static/js/main.2efd365b.chunk.js"
+  },
+  {
+    "revision": "4e1b5263ea643ba814ed",
+    "url": "https://morragan.github.io/kcalculate/static/js/runtime~main.a0cec8cb.js"
+  },
+  {
+    "revision": "f709403b0cd67a4a0f1f625541bbdaf0",
+    "url": "https://morragan.github.io/kcalculate/static/media/navlogo.f709403b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://localhost:3000/kcalculate/precache-manifest.bdb12f13995c53ef5b82bf245bb6dd13.js"
+  "https://morragan.github.io/kcalculate/precache-manifest.8467ba37f0106b87ace00b805fb57d6f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://localhost:3000/kcalculate/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://morragan.github.io/kcalculate/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
