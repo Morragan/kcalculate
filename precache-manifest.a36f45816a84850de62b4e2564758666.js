61bf1a5895dbadb4ecd0fc83bded3177b1a52181
Jakub Butkiewicz <jakub.butkiewicz@acaisoft.com>
2020-02-24 22:16:04 +0100

    Updates

---
+++ precache-manifest.a36f45816a84850de62b4e2564758666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a96e30197128925f5a69c2d28f3a7d3",
+    "url": "/diet-app/index.html"
+  },
+  {
+    "revision": "542a27f6151f34650d4e",
+    "url": "/diet-app/static/css/2.169712ab.chunk.css"
+  },
+  {
+    "revision": "01575d336b104737f660",
+    "url": "/diet-app/static/css/main.fcf1da21.chunk.css"
+  },
+  {
+    "revision": "542a27f6151f34650d4e",
+    "url": "/diet-app/static/js/2.e1c94a5e.chunk.js"
+  },
+  {
+    "revision": "01575d336b104737f660",
+    "url": "/diet-app/static/js/main.c7620e99.chunk.js"
+  },
+  {
+    "revision": "204ceb9bd6c7421ca052",
+    "url": "/diet-app/static/js/runtime~main.7c297397.js"
+  }
+]);
